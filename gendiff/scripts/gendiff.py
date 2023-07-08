e6b505426c1fd8fa3c2e8614906a621a83aa2321
alex <>
2023-07-08 12:55:38 +0300

    Step2: add format option description

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -10,10 +10,12 @@ def get_args_from_cmd_usage():
     description = "Compares two configuration files and shows a difference."
     arg1_description = "first file"
     arg2_description = "second file"
+    format_option_help = "set format of output"
 
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument(arg1_description)
     parser.add_argument(arg2_description)
+    parser.add_argument("-f", "--format", help=format_option_help)
 
     args = parser.parse_args()
 
